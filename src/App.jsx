cb71ecd16dade66b2d7d9c73fc057a3a9ebd3164
SukhithaSunil <sukithasunil@gmail.com>
2025-09-15 07:36:04 +0400

    base url corrections

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Navigate to="/social-portal" />} />
         <Route path="/social-portal" element={<Form />} />
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: '/community-aid-finance-portal/',
+  base: '/community-aid-finance-portal',
 })
